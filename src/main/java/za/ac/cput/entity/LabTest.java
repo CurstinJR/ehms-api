8c42ab614d3ec8d41a2b11098e13c2c11533b5a4
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-07 22:37:51 +0200

    Adding Getter into LabTest

--- src/main/java/za/ac/cput/entity/LabTest.java
+++ src/main/java/za/ac/cput/entity/LabTest.java
@@ -1,9 +1,32 @@
 package za.ac.cput.entity;
+/*
+LabTest.java
+Author:Kevin Lionel Mombo Ndinga(218180500)
+Date: 07 April 2022
+*/
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class LabTest {
     private long testId;
     private String test;
     private Date testSampleDate;
+    private BigDecimal testFee;
+
+    public long getTestId() {
+        return testId;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public Date getTestSampleDate() {
+        return testSampleDate;
+    }
+
+    public BigDecimal getTestFee() {
+        return testFee;
+    }
 }
