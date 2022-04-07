86724ad20661f43245e3494b89d7975963ecbfee
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-07 18:05:27 +0200

    Adding Getter,setter and toString in LoginCredentials

--- src/main/java/za/ac/cput/entity/LoginCredentials.java
+++ src/main/java/za/ac/cput/entity/LoginCredentials.java
@@ -4,4 +4,35 @@ public class LoginCredentials {
     private long id;
     private String email;
     private String password;
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "LoginCredentials{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
