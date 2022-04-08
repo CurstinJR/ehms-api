0225883c510f08a1ab8f1c573ef83846bbf1aacd
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-09 01:31:21 +0200

    testing the LoginCredentialsTest and it is working

---
+++ src/test/java/za/ac/cput/entity/LoginCredentialsTest.java
@@ -0,0 +1,32 @@
+package za.ac.cput.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginCredentialsTest {
+
+    @Test
+    public void test() {
+        LoginCredentials lct = new LoginCredentials.Builder()
+                .id(217895456)
+                .email("leonash@gmail.com")
+                .password("leonas7889")
+                .build();
+        System.out.println(lct);
+
+        LoginCredentials lct2 = new LoginCredentials.Builder()
+                .id(217895458)
+                .email("stevenB@gmail.com")
+                .password("stev7489")
+                .build();
+        System.out.println(lct2);
+
+        LoginCredentials lct3 = new LoginCredentials.Builder()
+                .id(217895554)
+                .email("spencerL@gmail.com")
+                .password("Lspen478")
+                .build();
+        System.out.println(lct3);
+    }
+}
