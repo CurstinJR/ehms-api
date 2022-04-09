a67ded8ced9d9156438c82f5e566dfe3bd027e3e
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-09 23:51:05 +0200

    Creating logincredentials repo and his interface

---
+++ src/main/java/za/ac/cput/repository/impl/ILoginCredentialsRepository.java
@@ -0,0 +1,8 @@
+package za.ac.cput.repository.impl;
+/*
+ILoginCredentialsRepository.java
+Author:Kevin Lionel Mombo Ndinga(218180500)
+Date: 09 April 2022
+*/
+public interface ILoginCredentialsRepository {
+}
---
+++ src/main/java/za/ac/cput/repository/impl/LoginCredentialsRepository.java
@@ -0,0 +1,9 @@
+package za.ac.cput.repository.impl;
+/*
+LoginCredentialsRepository.java
+Author:Kevin Lionel Mombo Ndinga(218180500)
+Date: 09 April 2022
+*/
+public class LoginCredentialsRepository {
+
+}
