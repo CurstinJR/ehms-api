b62e5fc55769e70cbca1344c7f245a1528426bad
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-09 16:53:04 +0200

    Testing Log credentials Test

---
+++ src/test/java/za/ac/cput/factory/LoginCredentialsFactoryTest.java
@@ -0,0 +1,29 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.LoginCredentials;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginCredentialsFactoryTest {
+
+    @Test
+    public void test() {
+
+        LoginCredentials login1 =  LoginCredentialsFactory.
+                createLogin(789941230,"Lancek@outlook.com","lance4789");
+        System.out.println(login1.toString());
+        assertNotNull(login1);
+
+        LoginCredentials login2 =  LoginCredentialsFactory.
+                createLogin(8745660,"michealj@gmail.com","MichealJ1230");
+        System.out.println(login2.toString());
+        assertNotNull(login2);
+
+        LoginCredentials login3 =  LoginCredentialsFactory.
+                createLogin(12458962,"StevenL@gmail.com","lS1203");
+        System.out.println(login3.toString());
+        assertNotNull(login3);
+    }
+
+}
