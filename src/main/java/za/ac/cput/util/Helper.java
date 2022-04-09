5fda0583a9e45d51a7fc994906a080197aa9bfd4
CurstinJR <cjrose13@protonmail.com>
2022-04-09 22:45:15 +0200

    Fix PR #50 conflict
    Fix conflict in the Helper.java
    
    PR: #50

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,19 +1,6 @@
 package za.ac.cput.util;
 
 import org.apache.commons.validator.EmailValidator;
-
-public class Helper {
-    public static boolean isNull(Long m) {
-      return(m== null || m.equals(""));
-    }
-    public static boolean isNullorEmpty(String k) {
-        return (k == null || k.equals("") || k.isEmpty() || k.equalsIgnoreCase("null"));
-
-    }
-        public static boolean isValidEmail(String email) {
-            EmailValidator ev = EmailValidator.getInstance();
-            return ev.isValid(email);
-        }
 import java.util.UUID;
 
 public class Helper
@@ -38,5 +25,4 @@ public class Helper
         EmailValidator ev = EmailValidator.getInstance();
         return ev.isValid(email);
     }
-
 }
