a7dc7c5ec8816561d71d76c7b386c457acad6a1e
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-09 15:13:16 +0200

    creating login objects

---
+++ src/main/java/za/ac/cput/factory/LoginCredentialsFactory.java
@@ -0,0 +1,10 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.entity.LoginCredentials;
+
+public class LoginCredentialsFactory {
+
+    public static LoginCredentials createLogin(long id, String email, String password) {
+
+    }
+}
