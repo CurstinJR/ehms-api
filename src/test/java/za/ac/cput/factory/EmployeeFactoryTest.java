0fa8475ba51db9edbc85511ac072c1a4fe072054
Tarren-Marc Adams <you@example.com>
2022-04-09 15:15:37 +0200

    Tested EmployeeFactory

--- src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -1,5 +1,6 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.entity.Employee;
 
@@ -13,6 +14,28 @@ class EmployeeFactoryTest {
         Employee employee = EmployeeFactory.createEmployee(3L,"Lyle","Adams");
         assertNotNull(employee);
         System.out.println("employee has been added");
+    }
+
+    @Test
+    void testEquality() {
+        Employee employee1 = EmployeeFactory.createEmployee(1L,"Lito","Oosthuizen");
+        Employee employee2 = EmployeeFactory.createEmployee(2L,"Carey","Koopman");
+        Employee employee3 = employee1;
+
+        assertEquals(employee1,employee3);
+        assertNotEquals(employee1,employee2);
+    }
+
+    @Test
+    void testIdentity(){
+        Employee employee1 = EmployeeFactory.createEmployee(1L,"Lito","Oosthuizen");
+        Employee employee2 = EmployeeFactory.createEmployee(2L,"Carey","Koopman");
+        Employee employee3 = employee1;
+
+        assertSame(employee1,employee3);
+        assertSame(employee1.getEmployeeId(),employee3.getEmployeeId());
+        assertNotSame(employee1,employee2);
+        assertNotSame(employee3.getEmployeeId(),employee2.getEmployeeId());
     }
 
 }
