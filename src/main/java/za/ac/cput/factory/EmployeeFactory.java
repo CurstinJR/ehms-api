fe90b9224b7eb3fdc7b688e15b27892397ed1a94
Tarren-Marc Adams <you@example.com>
2022-04-09 14:39:07 +0200

    Employee factory class

--- src/main/java/za/ac/cput/factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/factory/EmployeeFactory.java
@@ -1,5 +1,14 @@
 package za.ac.cput.factory;
 
-public class EmployeeFactory
-{
+import za.ac.cput.entity.Employee;
+import za.ac.cput.entity.LoginCredentials;
+
+public class EmployeeFactory {
+
+    public static Employee createEmployee (Long employeeId, String employeeName, String employeeSurname){
+        return new Employee.Builder().employeeId(employeeId)
+                .employeeName(employeeName)
+                .employeeSurname(employeeSurname)
+                .build();
+    }
 }
