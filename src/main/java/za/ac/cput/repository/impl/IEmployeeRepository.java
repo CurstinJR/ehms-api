44b820e243f9729574236aa64bbc716a6047487b
Tarren-Marc Adams <you@example.com>
2022-04-10 11:50:08 +0200

    EmployeeRepository interface created

--- src/main/java/za/ac/cput/repository/impl/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/repository/impl/IEmployeeRepository.java
@@ -1,6 +1,7 @@
 package za.ac.cput.repository.impl;
 
+import za.ac.cput.entity.Employee;
 import za.ac.cput.repository.IRepository;
 
-public interface IEmployeeRepository extends IRepository {
+public interface IEmployeeRepository extends IRepository<Employee,Long> {
 }
