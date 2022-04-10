bef84f49a6a8387ab923da478d8a548d30b66fda
Tarren-Marc Adams <you@example.com>
2022-04-10 12:05:40 +0200

    EmployeeRepository Created

--- src/main/java/za/ac/cput/repository/impl/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/repository/impl/IEmployeeRepository.java
@@ -3,5 +3,11 @@ package za.ac.cput.repository.impl;
 import za.ac.cput.entity.Employee;
 import za.ac.cput.repository.IRepository;
 
+/*
+IEmployeeRepository.java
+Author: Tarren-Marc Adams - 214041794
+Date: 10 April 2022
+ */
+
 public interface IEmployeeRepository extends IRepository<Employee,Long> {
 }
