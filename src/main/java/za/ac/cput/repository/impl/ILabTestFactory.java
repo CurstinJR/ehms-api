868bf645f840ac6d643e3680d57b4014c53fc685
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-10 21:52:29 +0200

    Adding the set<LabTest> GetAll()

--- src/main/java/za/ac/cput/repository/impl/ILabTestFactory.java
+++ src/main/java/za/ac/cput/repository/impl/ILabTestFactory.java
@@ -1,4 +1,11 @@
 package za.ac.cput.repository.impl;
 
-public interface ILabTestFactory {
+import za.ac.cput.entity.LabTest;
+import za.ac.cput.repository.IRepository;
+
+import java.util.Set;
+
+public interface ILabTestFactory extends IRepository<LabTestRepository,Long> {
+
+    public Set<LabTest> getAll();
 }
