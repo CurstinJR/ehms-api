61197e15bd0319e184c26a921e3d09b0bddeecb6
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-04-10 19:35:25 +0200

    updated version

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,11 +1,8 @@
 package za.ac.cput.repository;
 
+import java.util.Collection;
 import java.util.Optional;
-/*
-IRepository.java
-Author: Kevin Lionel Mombo Ndinga - 218180500
-Date: 9 April 2022
- */
+
 /*
 IRepository.java
 Author: Curstin Rose - 220275408
@@ -15,8 +12,8 @@ public interface IRepository<T, ID>
 {
     T save(T entity);
     Optional<T> findById(ID id);
-    Iterable<T> findAll();
-    T update(T entity, ID id);
-    void delete(T entity);
+    Collection<T> findAll();
+    T update(T entity);
+    boolean deleteById(ID id);
     boolean existsById(ID id);
 }
