3485d1585ea64bdd729d7dcf690917c8d80ac903
Tarren-Marc Adams <you@example.com>
2022-04-10 13:57:38 +0200

    Created Appointment Repository

--- src/main/java/za/ac/cput/factory/AppointmentFactory.java
+++ src/main/java/za/ac/cput/factory/AppointmentFactory.java
@@ -1,6 +1,7 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.entity.Appointment;
+import za.ac.cput.util.Helper;
 
 import java.time.LocalDate;
 
@@ -12,7 +13,10 @@ Date: 9 April 2022
 
 public class AppointmentFactory {
 
-    public static Appointment createApppointment (Long appointmentId, String appointmentTime, LocalDate appointmentDate){
+    public static Appointment createApppointment (String appointmentTime, LocalDate appointmentDate){
+
+        Long appointmentId = Helper.generateId();
+
         return new Appointment.Builder().appointmentId(appointmentId)
                 .appointmentTime(appointmentTime)
                 .appointmentDate(appointmentDate)
--- src/test/java/za/ac/cput/factory/AppointmentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AppointmentFactoryTest.java
@@ -18,15 +18,15 @@ class AppointmentFactoryTest {
 
     @Test
     public void addAppointment(){
-        Appointment appointment1 = AppointmentFactory.createApppointment(7L, "15:00", LocalDate.ofEpochDay(21-04-22));
+        Appointment appointment1 = AppointmentFactory.createApppointment("15:00",  LocalDate.ofEpochDay(21-04-22));
         assertNotNull(appointment1);
         System.out.println("Appointment created");
     }
 
     @Test
     void testEquality(){
-        Appointment appointment1 = AppointmentFactory.createApppointment(12L,"14:00", LocalDate.ofEpochDay(04-07-2011));
-        Appointment appointment2 = AppointmentFactory.createApppointment(13L,"14:00", LocalDate.ofEpochDay(04-07-2011));
+        Appointment appointment1 = AppointmentFactory.createApppointment("14:00", LocalDate.of(2022,03,12));
+        Appointment appointment2 = AppointmentFactory.createApppointment("14:00", LocalDate.of(2022,04,1));
         Appointment appointment3 = appointment1;
 
         assertEquals(appointment1,appointment3);
@@ -35,8 +35,8 @@ class AppointmentFactoryTest {
 
     @Test
     void testIdentity(){
-        Appointment appointment1 = AppointmentFactory.createApppointment(12L,"14:00", LocalDate.ofEpochDay(04-07-2011));
-        Appointment appointment2 = AppointmentFactory.createApppointment(13L,"10:00", LocalDate.ofEpochDay(04-07-2011));
+        Appointment appointment1 = AppointmentFactory.createApppointment("14:00", LocalDate.of(2022,03,12));
+        Appointment appointment2 = AppointmentFactory.createApppointment("14:00", LocalDate.of(2022,04,1));
         Appointment appointment3 = appointment1;
 
         assertSame(appointment1,appointment3);
