047649c77b05faee0914a63eccf8062d741d1fdd
CurstinJR <cjrose13@protonmail.com>
2022-10-17 23:48:33 +0200

    Add login authentication and fix role receptionist
    Reviewed-by: @CurstinJR

--- src/main/java/za/ac/cput/controller/auth/AuthController.java
+++ src/main/java/za/ac/cput/controller/auth/AuthController.java
@@ -1,7 +1,6 @@
 package za.ac.cput.controller.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,10 +29,11 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginCredentials loginCredentials) {
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginCredentials loginCredentials) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginCredentials.getEmail(), loginCredentials.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed-in successfully!", HttpStatus.OK);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok(auth.getPrincipal());
     }
 }
